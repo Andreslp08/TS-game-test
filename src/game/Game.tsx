5c298f158a582d0fb782ee26e7d345e5a9adace8
Andrés López <andreslp08@hotmail.com>
2021-11-17 16:18:08 -0500

    updated

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,32 +1,55 @@
 import { FrameManager } from "./FrameManager";
 import { GameTime } from "./GameTime";
 import Vector2 from "./math/Vector2";
-import Player from "./Player";
 
 export class Game {
 
 
     private frameManager: FrameManager;
-    private player: Player;
     private canvas: HTMLCanvasElement;
+    private mouseVector: Vector2;
+    private position:Vector2;
+    private velocity:Vector2;
+
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.frameManager = FrameManager.getInstance();
         this.GameLoop = this.GameLoop.bind(this);
-        this.player = new Player(new Vector2(0, 0));
+        this.position = new Vector2(0,50);
+        this.velocity = new Vector2(10,10);
     }
 
 
     public GameLoop(currentTime: number = 0): void {
         GameTime.deltaTime = (currentTime - this.frameManager.previousFrame);
+        const ctx = this.canvas.getContext('2d');
+        ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
+        this.position.addVector(this.velocity);
+        const radius = 40;
+        ctx.beginPath();
+        ctx.ellipse(this.position.x, this.position.y,radius, radius, 0, 0 , Math.PI *2, false)
+        ctx.fill();
+        const offX = this.position.x + radius *2;
+        const offY = this.position.y + radius *2;
+
+        if( offX > this.canvas.width){
+            this.velocity.substractVector(new Vector2(1,0))
+        }
+        else if( this.position.x < 0 +radius*2){
+            this.velocity.addVector(new Vector2(1,0))
+        }
+        if( offY > this.canvas.height){
+            this.velocity.substractVector(new Vector2(0,1))
+        }
+        else if( this.position.y < 0 +radius*2 ){
+            this.velocity.addVector(new Vector2(0,1))
+        }
         // console.log("1 second")
         // console.log(`current time: ${currentTime}`);
         // console.log(`previousFrame time: ${this.frameManager.previousFrame}`);
         // console.log(`delta: ${GameTime.deltaTime}`);
-        this.player.update(GameTime.deltaTime);
-        this.frameManager.previousFrame = currentTime;
-        this.player.draw(this.canvas, GameTime.deltaTime);
+
         window.requestAnimationFrame(this.GameLoop);
 
 
--- src/game/math/Vector2.tsx
+++ src/game/math/Vector2.tsx
@@ -8,18 +8,25 @@ export default class Vector2{
         this.y = y;
     }
     
-    public equalTo(vector:Vector2):boolean{
-        if(vector.x === this.x && vector.y === this.y){
-            return true;
-        }
-        return false;
-    }
-
     public static add(vectorOne:Vector2, vectorTwo:Vector2):Vector2{
         return new Vector2(
             vectorOne.x + vectorTwo.x,
             vectorOne.y + vectorTwo.y
         )
+    }
+
+    public static substract(vectorOne:Vector2, vectorTwo:Vector2):Vector2{
+        return new Vector2(
+            vectorOne.x - vectorTwo.x,
+            vectorOne.y - vectorTwo.y
+        )
+    }
+
+    public equalTo(vector:Vector2):boolean{
+        if(vector.x === this.x && vector.y === this.y){
+            return true;
+        }
+        return false;
     }
 
     public addVector(vector:Vector2):void{
@@ -28,12 +35,6 @@ export default class Vector2{
     }
 
 
-    public static substract(vectorOne:Vector2, vectorTwo:Vector2):Vector2{
-        return new Vector2(
-            vectorOne.x - vectorTwo.x,
-            vectorOne.y - vectorTwo.y
-        )
-    }
 
     public substractVector(vector:Vector2):void{
             this.x = this.x - vector.x;
@@ -45,9 +46,38 @@ export default class Vector2{
         this.y = this.y * vector.y;
     }
 
+    public divideVector(vector:Vector2):void{
+        this.x = this.x / vector.x;
+        this.y = this.y / vector.y;
+    }
+
+
     public multiplyByEscalar(escalar:number):void{
         this.x = this.x * escalar;
         this.y = this.y * escalar;
     }
+
+    public divideByEscalar(escalar:number):void{
+        this.x = this.x / escalar;
+        this.y = this.y / escalar;
+    }
+
+    public getMagnitude():number{
+        const x = this.x;
+        const y = this.y;
+        return Math.sqrt(x*x + y*y);
+    }
+
+    public normalize():void{
+        if(this.getMagnitude() > 0){
+            const m = this.getMagnitude();
+            this.x = this.x/m;
+            this.y = this.y/m;
+        }
+        console.log(this)
+      
+    }
+
+
 
 }
