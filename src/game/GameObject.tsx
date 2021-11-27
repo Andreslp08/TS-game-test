e43800416b29e6b8f6ecf1d4246fa56273e760a7
Andrés López <andreslp08@hotmail.com>
2021-11-27 00:47:14 -0500

    file name updated

--- src/game/GameObject.tsx
+++
@@ -1,4 +0,0 @@
-export default abstract class GameObject{
-    abstract draw(canvas:HTMLCanvasElement,deltaTime:number ):void;
-    abstract update(deltaTime:number):void;
-}
--- src/game/interfaces/transforms.tsx
+++
@@ -1,7 +0,0 @@
-import Vector2 from "../math/Vector2";
-
-export interface Transform{
-    position:Vector2
-    scale:Vector2
-    rotation:Vector2
-}
